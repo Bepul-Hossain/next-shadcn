ab96dde7309cd44f8aa208dc40f650071be3c2c6
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-02-05 21:56:41 +0600

    okay

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
-import { buttonVariants } from "@/components/ui/button"
 import Search from "./com/Search"
 
 interface propsType {
@@ -17,45 +12,13 @@ export function SiteHeader({ query, setQuery }: propsType) {
     <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
       <div className="site-header container h-12">
         <div>
-          <MainNav items={siteConfig.mainNav} />
+          <MainNav />
         </div>
         <div>
           <Search query={query} setQuery={setQuery} />
         </div>
         <div className="flex items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                  className: "text-slate-700 dark:text-slate-400",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                  className: "text-slate-700 dark:text-slate-400",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
             <ThemeToggle />
           </nav>
         </div>
