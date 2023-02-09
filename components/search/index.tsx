94bfe44761044cab820327e6c3debb242233dce4
Bepul-Hossain <bepul.cse.ru@gmail.com>
2023-02-09 18:53:52 +0600

    refactor

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,26 +1,3 @@
-import React from "react";
-
-type InputProps = {
-    query?: string;
-    setQuery:React.Dispatch<React.SetStateAction<string>>
-}
-
-const Search =(props:InputProps)=>{
-    const { query,setQuery } = props;
-
-    const inputHandleChange=(event: React.ChangeEvent<HTMLInputElement>)=>{
-        setQuery(event.target.value);
-        console.log(query)
-      }
-
-    return(
-        <input
-        type='text'
-        value={query}
-        onChange = {inputHandleChange}
-        placeholder = "Search..."
-        />
-    )
-}
+import Search from './Search'
 
 export default Search
---
+++ components/songs/Songs.tsx
@@ -0,0 +1,43 @@
+import Item from "@/data/dataTypes"
+
+import Song from "../song"
+
+// type UserType = {
+//     name?: string;
+//     videoLink?: string;
+//     lyrics?: string;
+//   };
+// interface Props{
+//     data: UserType
+// }
+// interface UserProps {
+//     data?: Item[],
+// }
+
+type UserProps = {
+  data?: Item[]
+  query: string
+}
+
+const Songs = (props: UserProps) => {
+  const { data, query } = props
+  return (
+    <div>
+      {data
+        ?.filter((item) =>
+          item?.name?.toLowerCase().includes(query.toLowerCase())
+        )
+        .map((song, index) => (
+          <Song
+            name={song.name}
+            videoLink={song.videoLink}
+            lyrics={song.lyrics}
+            index={++index}
+            key={song.name}
+          />
+        ))}
+    </div>
+  )
+}
+
+export default Songs
